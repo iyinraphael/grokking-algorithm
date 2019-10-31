47643a89f94009ebaedc5f593b0ba86b77a39be4
iyinraphael <iyinraphael@gmail.com>
2019-10-31 12:09:19 -0700

    bubble sort

--- selection-sort.playground/Contents.swift
+++ selection-sort.playground/Contents.swift
@@ -29,6 +29,46 @@ func selectionSort(_ arr:[Int] ) -> [Int] {
     return newArr
 }
 
-let numbers = [5, 3, 6, 2, 10]
+var numbers = [5, 3, 6, 2, 10]
 
-selectionSort(numbers)
+func newSelectionSort(_ array: inout [Int]){
+    
+    for current in 0..<(array.count - 1) {
+        var lowest = current
+        
+        for other in (current + 1)..<array.count {
+            if array[lowest] > array[other] {
+                lowest = other
+            }
+        }
+        if lowest != current {
+            array.swapAt(lowest, current)
+        }
+        
+    }
+}
+newSelectionSort(&numbers)
+
+//Bubble Sort
+
+func bubbleSort(_ array: inout [Int]) {
+    guard array.count >= 2 else {return}
+    
+    for end in (1..<array.count).reversed() {
+        var swapped = false
+        
+        for current in 0..<end {
+            if array[current] > array[current + 1] {
+                array.swapAt(current, current + 1)
+                swapped = true
+            }
+            if !swapped {return}
+        }
+    }
+}
+
+bubbleSort(&numbers)
+
+//for end in (1..<numbers.count).reversed() {
+//    print(end)
+//}
