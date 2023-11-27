0e3eb7cf8b61c071ffbaf00d4428bc3b21d1c37c
iyinrapahel <iyinraphael@gmail.com>
2023-11-27 09:22:28 -0500

    recursion example

---
+++ recursion.py
@@ -0,0 +1,44 @@
+# Grandma artic box problem
+
+# First approach pseudocode
+
+
+def look_for_key(main_box):
+    pile = main_box.make_a_pile_to_look_through()
+    while pile is not empty:
+        box = pile.grab_a_box()
+        for item in box:
+            if item.is_a_box():
+                pile.append(item)
+            elif item.is_a_key():
+                print "found the key!"
+
+
+
+# Second approach pseudocode
+
+
+def look_for_key(box):
+    for item in box:
+        if item.is_a_box():
+            look_for_key(item)
+        elif item.is_a_key():
+            print "found the key!"
+
+
+def countdown(i):
+    print(i)
+    if i <= 1:
+        return
+    else:
+        countdown(i - 1)
+
+
+def fact(x):
+    if x == 1:
+        return 1
+    else:
+        return x * fact(x-1)
+
+
+
